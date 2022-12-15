5c2aa2c25d82056e092f4d86d0b3a9339749c5fc
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2022-12-15 11:56:20 +0100

    Change API, to allow Logging in Plugin Constructor

--- src/main/java/com/c0d3m4513r/pluginapi/API.java
+++ src/main/java/com/c0d3m4513r/pluginapi/API.java
@@ -4,11 +4,14 @@ import com.c0d3m4513r.pluginapi.command.CommandRegistrar;
 import com.c0d3m4513r.pluginapi.command.CommandResult;
 import com.c0d3m4513r.pluginapi.config.MainConfig;
 import com.c0d3m4513r.pluginapi.config.iface.IConfigLoaderSaver;
+import com.c0d3m4513r.plugindef.Plugin;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.SneakyThrows;
 import org.slf4j.Logger;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URLClassLoader;
 
 public abstract class API {
@@ -25,15 +28,17 @@ public abstract class API {
     @Getter
     protected static CommandResult commandResult;
 
-    @SneakyThrows
-    protected API(){
+    protected static void loadMain(){
         try(URLClassLoader loader = URLClassLoader.newInstance(
                 new java.net.URL[]{API.class.getProtectionDomain().getCodeSource().getLocation()},
-                getClass().getClassLoader())) {
+                API.class.getClassLoader())) {
                 config = (MainConfig) loader
                 .loadClass(com.c0d3m4513r.plugindef.Plugin.configClass)
                 .getConstructor()
                 .newInstance();
+        } catch (IOException | ClassNotFoundException | InvocationTargetException | InstantiationException |
+                 IllegalAccessException | NoSuchMethodException e) {
+            throw new RuntimeException("Could not create Main Plugin Instance, for plugin '"+ Plugin.name+"' and it's supposed main at:"+Plugin.configClass,e);
         }
     }
     public static void runOnMain(Runnable run){
